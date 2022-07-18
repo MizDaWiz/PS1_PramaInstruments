1cb9b7b7923696069da0e2485070106cc6e16b1f
Saksham Yadav <ssysaksham@gmail.com>
2022-07-18 12:09:50 +0530

    ESP Version Completed
    This code is not tested nor calibrated.
    Consider this just as a pseudocode.
    A lot of calibration and optimisation between the code and the data from the data from the website is required.

--- SolarTracker/Final_Code/FinalCode_For_ESPVersion/FinalCode_For_ESPVersion.ino
+++ SolarTracker/Final_Code/FinalCode_For_ESPVersion/FinalCode_For_ESPVersion.ino
@@ -14,26 +14,38 @@
     Twitter: https://twitter.com/witnessmenow
  *******************************************************************/
 /*Updated for personal use by Saksham Yadav
-https://github.com/sakshamssy  */
+  https://github.com/sakshamssy  */
+/* Get all possible data from MPU6050
+   Accelerometer values are given as multiple of the gravity [1g = 9.81 m/s²]
+   Gyro values are given in deg/s
+   Angles are given in degrees
+   Note that X and Y are tilt angles and not pitch/roll.
 
-// ----------------------------
-// Standard Libraries
-// ----------------------------
+   License: MIT
+*/
+int ENB = 9; //For Enable Pin of Motor Driver
+int al1 = 10; //For Motor Driver Input Pin1
+int al2 = 11; //For Motor Driver Input Pin2
+#include "Wire.h"
+#include <MPU6050_light.h>
+MPU6050 mpu(Wire);
+long timer = 0;
 
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
-
 //------- Replace the following! ------
 char ssid[] = "Galaxy M31s9DF6";         // your network SSID (name)
 char password[] = "ogzn5578"; // your network key
-char t[]="";
+float mindiff = 1;
+float diff;
+char t[] = "";
 // For Non-HTTPS requests
 // WiFiClient client;
 
 // For HTTPS requests
 WiFiClientSecure client;
 
-// Just the base of the URL you want to connect to 
+// Just the base of the URL you want to connect to
 
 #define TEST_HOST "api.thingspeak.com"
 // OPTIONAL - The finferprint of the site you want to connect to.
@@ -42,7 +54,10 @@ WiFiClientSecure client;
 
 void setup()
 {
-
+  pinMode(ENB, OUTPUT);
+  pinMode(al1, OUTPUT); // Configure pin 10 as an Output
+  pinMode(al2, OUTPUT); // Configure pin 11 as an Output
+  digitalWrite(ENB, HIGH); ///for Actuator
   Serial.begin(115200);
 
   // Connect to the WiFI
@@ -72,8 +87,23 @@ void setup()
 
   // If you want to check the fingerprint
   client.setFingerprint(TEST_HOST_FINGERPRINT); //for https
-//  client.setInsecure();  //for http
-  makeHTTPRequest("/apps/thinghttp/send_request?api_key=DI3UPIO33ATKCCW6");
+  //  client.setInsecure();  //for http
+  //makeHTTPRequest("/apps/thinghttp/send_request?api_key=DI3UPIO33ATKCCW6");
+
+
+  //MPU6050 setup
+  Wire.begin();
+
+  byte status = mpu.begin();
+  Serial.print(F("MPU6050 status: "));
+  Serial.println(status);
+  while (status != 0) { } // stop everything if could not connect to MPU6050
+
+  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  delay(1000);
+  mpu.calcOffsets(true, true); // gyro and accelero
+  Serial.println("Done!\n");
+
 }
 
 float makeHTTPRequest(char address[])
@@ -125,38 +155,64 @@ float makeHTTPRequest(char address[])
 
   // While the client is still availble read each
   // byte and print to the serial monitor
-  int i=0;
+  int i = 0;
   while (client.available())
   {
     char c = 0;
     client.readBytes(&c, 1);
-   t[i]=c;
+    t[i] = c;
     //Serial.print(c);
-    i=i+1;
+    i = i + 1;
   }
-  for(int j=0;j<=strlen(t);j++)
+  for (int j = 0; j <= strlen(t); j++)
   {
-    if (!(t[i]<58 && t[i]>46))
+    if (!(t[i] < 58 && t[i] > 46))
     {
-      t[i]='\0';
+      t[i] = '\0';
     }
   }
-//Serial.print('\n');
-   float val;
-   char str[4];
-   
-   strcpy(str, t);
-   val = atof(str);
-return val;
+  //Serial.print('\n');
+  float val;
+  char str[4];
+
+  strcpy(str, t);
+  val = atof(str);
+  return val;
 }
 //apps/thinghttp/send_request?api_key=DI3UPIO33ATKCCW6
 void loop()
 {
   Serial.println("New Reading:");
 
-//makeHTTPRequest("/apps/thinghttp/send_request?api_key=NT3TFKEXNIBK86SY");
-  float m=makeHTTPRequest("/apps/thinghttp/send_request?api_key=DI3UPIO33ATKCCW6");
-    Serial.println(m);//m here is the Solar Altitude 
-   delay(2000);
-//Pseudo  
+  //makeHTTPRequest("/apps/thinghttp/send_request?api_key=NT3TFKEXNIBK86SY");
+  float m = makeHTTPRequest("/apps/thinghttp/send_request?api_key=DI3UPIO33ATKCCW6");
+  Serial.println(m);//m here is the Solar Altitude
+  delay(2000);
+  mpu.update();
+  float z = mpu.getAngleZ();
+  Serial.print("\tZ: "); Serial.println(mpu.getAngleZ());
+  diff = m - z;
+
+  if (abs(diff) > mindiff)
+  {
+    if ((diff) > 0)
+    {
+      digitalWrite(al1, HIGH);
+      digitalWrite(al2, LOW);
+      Serial.println("up");
+    }
+    else if ((diff) < 0)
+    {
+      digitalWrite(al1, LOW);
+      digitalWrite(al2, HIGH);
+      Serial.println("down");
+    }
+  }
+  else
+  {
+    digitalWrite(al1, LOW);
+    digitalWrite(al2, LOW);
+    Serial.println("No need to optimise");
+  }
+
 }
